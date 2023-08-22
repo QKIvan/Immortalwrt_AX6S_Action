b7a86e547f4aeaeac60babbc06ffc50977d25985
QKIvan <ivan33@163.com>
2023-08-22 19:58:21 +0800

    CI: Add support for ImmortalWrt 18.06 CS & Regen config
    Co-authored-by: CCA2878 <51525400+CCA2878@users.noreply.github.com>

---
+++ diy-part1-2305.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+rm -rf feeds.conf.default
+
+# Add a feed source
+echo 'src-git packages https://github.com/immortalwrt/packages;openwrt-23.05' >>feeds.conf.default
+echo 'src-git luci https://github.com/immortalwrt/luci;openwrt-23.05' >>feeds.conf.default
+echo 'src-git routing https://github.com/openwrt/routing.git' >>feeds.conf.default
+echo 'src-git telephony https://github.com/openwrt/telephony.git' >>feeds.conf.default
